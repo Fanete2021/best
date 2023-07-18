8d6dbfa4e6cbb0e5023471729983272bdd20b1a7
Fanete <Fanete2021@gmail.com>
2023-07-19 00:00:15 +0700

    Наполнил реактом

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,9 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+  const filteredMods: string[] = Object.entries(mods)
+    .filter(([, flag]) => Boolean(flag))
+    .map(([className]) => className);
+
+  return [cls, ...filteredMods, ...additional].join(' ');
+}
