666382106b285622080c2da543f3fd5aa2a36220
Fanete <Fanete2021@gmail.com>
2023-07-19 17:31:16 +0700

    Добавил Navbar

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,13 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
   const filteredMods: string[] = Object.entries(mods)
     .filter(([, flag]) => Boolean(flag))
     .map(([className]) => className);
 
-  return [cls, ...filteredMods, ...additional].join(' ');
+  return [
+    cls,
+    ...filteredMods,
+    ...additional.filter(Boolean)
+  ].join(' ');
 }
