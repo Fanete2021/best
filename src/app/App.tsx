b7d2d0abb7251111f8d24d14a8329408928cf9a8
Fanete <Fanete2021@gmail.com>
2023-07-19 16:39:26 +0700

    Вынес логику react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import { useTheme } from './providers/ThemeProvider';
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -16,12 +17,7 @@ const App = () => {
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />}/>
-          <Route path={'/'} element={<MainPage />}/>
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
