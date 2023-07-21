f6d1e76ed267c2d8250994ce129e3962250ee556
Fanete <Fanete2021@gmail.com>
2023-07-21 17:21:06 +0700

    Добавил error boundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './PageError.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui';
+
+interface PageErrorProps {
+  className?: string;
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    location.reload();
+  };
+
+  return (
+    <div className={classNames(styles.PageError, {}, [ className ])} >
+      <p>
+        {t('Произошла непредвиденная ошибка')}
+      </p>
+
+      <Button
+        theme={ThemeButton.CLEAR}
+        onClick={reloadPage}
+      >
+        {t('Обновить страницу')}
+      </Button>
+    </div>
+  );
+};
